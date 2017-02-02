964039696e3eefebcc8424e046b3cad864e078ea
christos95 <christosvtecpower@gmail.com>
2017-02-02 10:49:29 +0000

    Change default location from Fernhurst to Litochoro Also add a buuton to get us to any latitude and longitude the user put

--- app/src/main/java/com/example/a2papac92/mapping/HelloMap.java
+++ app/src/main/java/com/example/a2papac92/mapping/HelloMap.java
@@ -3,12 +3,15 @@ package com.example.a2papac92.mapping;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-public class HelloMap extends Activity
+public class HelloMap extends Activity implements View.OnClickListener
 {
     MapView mv;
 
@@ -16,6 +19,9 @@ public class HelloMap extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(this);
 
         Configuration.getInstance().load
                 (this, PreferenceManager.getDefaultSharedPreferences(this));
@@ -25,4 +31,19 @@ public class HelloMap extends Activity
         mv.getController().setZoom(14);
         mv.getController().setCenter(new GeoPoint(40.1,22.5));
     }
+
+    @Override
+    public void onClick(View view) {
+        EditText latitudeEditText = (EditText) findViewById(R.id.latitudeEditText);
+        double latitude = Double.parseDouble(latitudeEditText.getText().toString());
+
+        EditText longitudeEditText = (EditText) findViewById(R.id.longitudeEditText);
+        double longitude = Double.parseDouble(longitudeEditText.getText().toString());
+
+        mv.getController().setCenter(new GeoPoint(latitude,longitude));
+    }
 }
+
+
+// xylophagou latitude 34.975
+//            longitude 33.8517
