44f52658f67a83c9419393e482204359032f3c99
christos95 <christosvtecpower@gmail.com>
2017-02-06 16:57:07 +0000

    2 activities

--- app/src/main/java/com/example/a2papac92/mapping/HelloMap.java
+++ app/src/main/java/com/example/a2papac92/mapping/HelloMap.java
@@ -11,6 +11,14 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import android.content.Intent;
+
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+
 public class HelloMap extends Activity implements View.OnClickListener
 {
     MapView mv;
@@ -32,6 +40,12 @@ public class HelloMap extends Activity implements View.OnClickListener
         mv.getController().setCenter(new GeoPoint(40.1,22.5));
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_hello_map, menu);
+        return true;
+    }
+
     @Override
     public void onClick(View view) {
         EditText latitudeEditText = (EditText) findViewById(R.id.latitudeEditText);
@@ -42,4 +56,38 @@ public class HelloMap extends Activity implements View.OnClickListener
 
         mv.getController().setCenter(new GeoPoint(latitude,longitude));
     }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.choosemap)
+        {
+            Intent intent = new Intent(this,MapChooseActivity.class);
+            startActivityForResult(intent,0);
+
+            return true;
+        }
+        return false;
+    }
+
+    protected void onActivityResult(int requestCode,int resultCode,Intent intent)
+    {
+
+        if(requestCode==0)
+        {
+
+            if (resultCode==RESULT_OK)
+            {
+                Bundle extras=intent.getExtras();
+                boolean cyclemap = extras.getBoolean("com.example.cyclemap");
+                if(cyclemap==true)
+                {
+                    mv.setTileSource(TileSourceFactory.CYCLEMAP);
+                }
+                else
+                {
+                    mv.getTileProvider().setTileSource(TileSourceFactory.MAPNIK);
+                }
+            }
+        }
+    }
+
 }
